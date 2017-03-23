cfdef7b79482fde37b87d42e91102ce4628afee2
cleotracey <cleotracey@hotmail.com>
2017-03-22 17:35:05 -0700

    forgot customer oops

---
+++ cs304/src/Customer.java
@@ -0,0 +1,15 @@
+/**
+ * Created by melaniereardon on 2017-03-22.
+ */
+import
+
+
+
+public class Customer {
+    private String name;
+    private int customerID;
+    private int phoneNumber;
+    private String address;
+    private List<Event> events;
+    private List<Ticket> tickets;
+}
